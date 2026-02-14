dbb733d06f3a49909342f24ea9be8d0e23d6f2fa
Jose Vasquez <joseevb@protonmail.com>
2026-02-14 18:53:42 +0100

    feat: update scripts and migration logic

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,7 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
-import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
 const sqlite = new Database(process.env.DB_FILE_NAME!);
 const db = drizzle(sqlite);
-migrate(db, { migrationsFolder: "./drizzle" });
 
 export { db };
